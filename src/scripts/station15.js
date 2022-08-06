3e0b898215aa3073805d3e6bcb6504f483da4b36
momongya <61648667+momongya@users.noreply.github.com>
2022-08-07 05:30:24 +0900

    Update station15.js

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,15 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const time = hoge => new Promise(resolve => setTimeout(() => resolve(), hoge));
+  const result = await userList.map(test);
+  await time(3000);
   return await result
 }
 
-function test() {
-  return
+function test(userList) {
+  userList.full_name = userList.family_name + " " + userList.first_name;
+  console.log(userList);
+  return userList;
 }
 
